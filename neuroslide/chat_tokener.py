61180ccd0e787dc3fdd3345258636e2114f75a26
Mikhail <smirnow.mishas2017@gmail.com>
2024-09-07 14:49:25 +0600

    edit-tokener

--- neuroslide/chat_tokener.py
+++ neuroslide/chat_tokener.py
@@ -1,5 +1,8 @@
+# -*- coding: utf-8 -*-
 from datetime import datetime, timedelta
+
 import requests
+
 
 class Tokener:
     """
@@ -10,7 +13,6 @@ class Tokener:
         token_end_time (datetime): The time when the current token will expire.
         api_key (str): The API key used to request the access token.
     """
-
     def __init__(self, api_key):
         """
         Initializes the Tokener with the provided API key.
@@ -22,31 +24,35 @@ class Tokener:
         self.token_end_time = None
         self.api_key = api_key
 
-    def get_access_token(self,):
+    def get_access_token(
+        self,
+    ):
         """
         Requests a new access token from the Yandex Cloud IAM service.
         """
         url = "https://iam.api.cloud.yandex.net/iam/v1/tokens"
         headers = {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
         }
-        data = {
-            'yandexPassportOauthToken': self.api_key
-        }
+        data = {"yandexPassportOauthToken": self.api_key}
 
-        response = requests.request("POST", url, headers=headers, json=data, verify=False)
+        response = requests.request(
+            "POST", url, headers=headers, json=data, verify=False
+        )
         response_data = response.json()
 
-        self.token = response_data['iamToken']
-        self.token_update_time = datetime.datetime.now() + timedelta(hours=1)
+        self.token = response_data["iamToken"]
+        self.token_update_time = datetime.now() + timedelta(hours=1)
 
-    def get_token(self,):
+    def get_token(
+        self,
+    ):
         """
         Retrieves the current access token, refreshing it if necessary.
 
         Returns:
             str: The current access token.
         """
-        if self.token == None or datetime.datetime.now() >= self.token_update_time:
+        if self.token == None or datetime.now() >= self.token_update_time:
             self.get_access_token()
         return self.token
